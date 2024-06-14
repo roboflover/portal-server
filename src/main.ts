687975eddbf6832a12158455354da31d24d44035
roboflover <96958044+roboflover@users.noreply.github.com>
2024-06-14 03:24:16 +0300

    sss

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   }
   app.enableCors();
   await app.listen(8080); 
-  console.log(`url ${frontendUrl}`)
+  console.log(`frontend url ${frontendUrl}`)
 }
 bootstrap();
